d24f3fa965972193b03692004df6f1c10db5745c
quinnmike82 <87664191+quinnmike82@users.noreply.github.com>
2023-02-20 20:54:55 +0700

    fix: add documentgroup
    Document m-t-m Group, update constraint prevent unique combination rows

--- src/document/entities/document.entity.ts
+++ src/document/entities/document.entity.ts
@@ -2,6 +2,8 @@ import {
   DocType,
   Document as DocumentEntity,
   IssueMarkType,
+  DocumentReceiveDetail as DocumentReceiveDetailEntity,
+  DocumentReceiveGroup as DocumentReceiveGroupEntity,
 } from '@prisma/client';
 export class Document implements DocumentEntity {
   id: string;
@@ -15,4 +17,24 @@ export class Document implements DocumentEntity {
   content: string;
   createdAt: Date;
   updatedAt: Date;
+}
+export class DocumentReceiveDetail implements DocumentReceiveDetailEntity  {
+  id: string
+  documentId: string
+  userId: string
+  date_receive: Date
+  isRead: boolean
+  isDeleted: boolean
+  createdAt: Date
+  updatedAt: Date
+}
+export class DocumentReceiveGroup implements DocumentReceiveGroupEntity  {
+  id: string
+  documentId: string
+  groupId: string;
+  date_receive: Date
+  isRead: boolean
+  isDeleted: boolean
+  createdAt: Date
+  updatedAt: Date
 }
